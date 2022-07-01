5054d661baa44d09a3349023ae24d1ca2b89c3e5
Enoch <madehinenoch@gmail.com>
2022-06-30 21:09:45 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
+char c;
+int i;
+long int l;
+long long int ll;
+float d;
+char c;
 printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
