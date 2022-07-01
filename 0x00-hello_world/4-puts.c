f96f3cbb9b54482cd1b544358fa2677aaabc62ce
Enoch <madehinenoch@gmail.com>
2022-06-30 20:48:14 -0700

    Programming in C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle,"
+ * use the function puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
+/**
+ * main - prints exactly "the words used in the project"
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is apiece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
