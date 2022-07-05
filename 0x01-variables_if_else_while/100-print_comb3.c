28e9f42355bfb36133999f0d3bfe98b53b658ff6
Enoch <madehinenoch@gmail.com>
2022-07-05 01:10:39 -0700

    programming in c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,32 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 /**
- * main - prints all possible different combinations of two digits
- * Return: Always 0
+ *main - assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 int ch;
 int n;
 for (ch = 48; ch <= 57; ch++)
+{
 for (n = 49; n <= 57; n++)
 {
 if (n > ch)
+{
 putchar(ch);
 putchar(n);
-}
 if (ch != 56 || n != 57)
 {
 putchar(44);
 putchar(32);
 }
-putchar(10);
+}
+}
+}
+
+putchar(10); /* this is an ascii code for new line*/
+
 return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
- * Return: Always 0
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-for (n = 48; n <= 57; n++)
+int ch;
+for (ch = 48; ch <= 57; ch++)
 {
-putchar(n);
-}
-if (n != 57)
+
+putchar(ch);
+if (ch != 57)
 {
 putchar(44);
 putchar(32);
 }
-putchar(10);
+}
+
+putchar(10); /* this is an ascii code for new line*/
+
 return (0);
 }
+
