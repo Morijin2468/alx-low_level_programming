0661bf0cf9162ddf80b38132d4b2a546a7e01c5a
Enoch <madehinenoch@gmail.com>
2022-07-14 20:22:52 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
- * * reverse_array -> reversing an array
- * * @a: array a
- * * @n: an element of an array
- */
+ * reverse_array - reverse a int array
+ * @a: pointer
+ * @n: array length
+ * Return: void
+*/
+
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int i = 0, half;
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < i / 2; k++)
+	for (half = n / 2; half > 0; half--, i++)
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
 	}
 }
